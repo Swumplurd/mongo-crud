02c983d9625a956a09ce7bdad5902ead5c342aef
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-06-03 18:23:59 -0500

    se creo la conexion a la base de datos

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uri = 'mongodb://localhost/db_recursos';
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error en la conexion a MongoDB'));
+db.once('open', function callback() {
+    console.log("Conectado!");
+});
+
+module.exports = db;
